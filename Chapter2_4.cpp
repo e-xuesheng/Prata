b2823c0d7015b3bd8ebd1b7340c76e9ca3ef7a08
e-xuesheng <54906639+e-xuesheng@users.noreply.github.com>
2019-09-05 18:32:35 +0300

    Chapter2_4

---
+++ Chapter2_4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main(void){
+int years,months;
+std::cout<<"Please, enter your age: ";
+std::cin>>years;
+std::cout<<" press enter, please \n";
+months=years*12;
+std::cout<<"Months= "<<months;
+return 0;
+}
