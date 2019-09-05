d10e051fda05afd2f2900f191384c50a7249be26
e-xuesheng <54906639+e-xuesheng@users.noreply.github.com>
2019-09-05 19:09:53 +0300

    Delete Chapter2_2.cpp

--- Chapter2_2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main(){
-int furlong,yards;
-std::cout<<"Please enter the distance in furlongs:  ";
-std::cin>>furlong;
-yards=furlong*220;
-std::cout<<" Yards= "<<yards;
-return 0;
-}
