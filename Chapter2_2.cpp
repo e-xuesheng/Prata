00d54fcd3790c7dff262165f02c71fa3a11d9df1
e-xuesheng <54906639+e-xuesheng@users.noreply.github.com>
2019-09-05 13:11:50 +0300

    Task to Exercise 2 in Chapter 2

---
+++ Chapter2_2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+int furlong,yards;
+std::cout<<"Please enter the distance in furlongs:  ";
+std::cin>>furlong;
+yards=furlong*220;
+std::cout<<" Yards= "<<yards;
+return 0;
+}
